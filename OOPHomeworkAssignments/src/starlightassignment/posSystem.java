ab238aae219c20fc4ca46817fc9de77862acbcd0
Brian <bfeddes@hotmail.com>
2022-09-23 22:46:45 -0500

    initial commit for POS assignment

---
+++ OOPHomeworkAssignments/src/starlightassignment/posSystem.java
@@ -0,0 +1,24 @@
+package starlightassignment;
+
+public class posSystem {
+    public static void main(String args[]){
+        /*TO-DO LIST:
+         * Start program with a header
+         * Ask the user to enter their name
+         * Offer choices for the user to select
+         * Calculate the user's total bill depending on the choices the user makes
+         * Implement a tip percentage that will be added on to their bill
+         * Apply the 10% discount
+         */
+
+    System.out.println("*************************************");
+    System.out.println("STARLIGHT COFFEE POINT-OF-SALE SYSTEM");
+    System.out.println("*************************************");
+
+
+
+
+
+    }
+    
+}
