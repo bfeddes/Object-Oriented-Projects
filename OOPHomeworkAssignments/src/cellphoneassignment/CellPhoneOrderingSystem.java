b4ef21c93826152e1d0701b0b3237e13021a1a62
Your Name <you@example.com>
2022-10-01 14:06:24 -0500

    Need to seperate each phone's specs

--- OOPHomeworkAssignments/src/cellphoneassignment/CellPhoneOrderingSystem.java
+++ OOPHomeworkAssignments/src/cellphoneassignment/CellPhoneOrderingSystem.java
@@ -12,6 +12,79 @@ public class CellPhoneOrderingSystem {
      * Create method for asking what amount of storage (64 GB 128 GB 256 GB)
      * 
      */
+
+    // Type of phone method
+    public static double phoneCalculator(int numOfPhones){
+        // Initializing scanner class
+        Scanner sc = new Scanner(System.in);
+        // Initializing and declaring variables outside of loops
+        String phoneType = "";
+        int iphoneCost = 849;            
+        int androidCost = 799;
+        double totalCost = 0;
+        // Asking the user how many phones they want to purchase
+        // Asking the user what type of phone they want
+        for (int i = 1; i <=numOfPhones; i++){
+        System.out.printf("Lets configure phone #%d\n",i);
+        System.out.print("Enter A for Android or I for iPhone: ");
+        phoneType = sc.next();
+        // Options for an iPhone
+        if (phoneType.equalsIgnoreCase("I")){
+        totalCost += iphoneCost;
+        phoneType = "IPhone";
+        } else if (phoneType.equalsIgnoreCase("A")){
+                totalCost += androidCost;
+            phoneType = "Android";
+        }
+        // Asking the screen size
+        System.out.print("What screen size? Enter 1 for 5.6\", 2 for 6.2\", or 3 for 6.7\": ");
+        int screenSize = sc.nextInt();
+        if (screenSize == 1){
+        } else if (screenSize == 2){
+            totalCost *= 1.2;
+        } else if (screenSize == 3){
+            totalCost *= 1.4;
+        }
+        // Asking the storage
+        System.out.print("How much storage? Enter 64, 128, 256: ");
+        int storage = sc.nextInt();
+        if (storage == 64){
+        } else if (storage == 128){
+            totalCost += 100;
+        } else if (storage == 256){
+            totalCost += 250;
+        }
+        // Asking the user what add-ons they want
+        System.out.println("What add-on do you want?");
+        System.out.print("   [C]ase ($49)\n   [S]creen protector ($15)\n   [E]ar buds ($99)\n   [W]ireless charger ($59)\n   [F]inish order\nEnter the letter of your choice: ");
+        String addOnChoice = sc.next();
+        if(addOnChoice.equalsIgnoreCase("C")){
+            totalCost += 49;
+        }else if(addOnChoice.equalsIgnoreCase("S")){
+            totalCost += 15;
+        }else if(addOnChoice.equalsIgnoreCase("E")){
+            totalCost += 99;
+        }else if(addOnChoice.equalsIgnoreCase("W")){
+            totalCost += 59;
+        }
+        // While loop for add-ons while the user doesn't select finish
+        while (!addOnChoice.equalsIgnoreCase("F")){
+        System.out.println("What add-on do you want?");
+        System.out.print("   [C]ase ($49)\n   [S]creen protector ($15)\n   [E]ar buds ($99)\n   [W]ireless charger ($59)\n   [F]inish order\nEnter the letter of your choice: ");
+        addOnChoice = sc.next();
+        if(addOnChoice.equalsIgnoreCase("C")){
+        totalCost += 49;
+        }else if(addOnChoice.equalsIgnoreCase("S")){
+            totalCost += 15;
+        }else if(addOnChoice.equalsIgnoreCase("E")){
+            totalCost += 99;
+        }else if(addOnChoice.equalsIgnoreCase("W")){
+            totalCost += 59;
+        }
+        }
+     }
+     return totalCost;
+}
     // Method to print out the header
     public static void header(){
         System.out.println("****************************************************");
@@ -26,76 +99,10 @@ public class CellPhoneOrderingSystem {
         Scanner sc = new Scanner(System.in);
         // Printing the header
         header();
-        // Initializing and declaring variables outside of loops
-        String phoneType = "";
-        int iphoneCost = 849;
-        int androidCost = 799;
-        int totalCost = 0;
-        // Asking the user how many phones they want to purchase
         System.out.print("How many phones will you purchase? ");
         int numOfPhones = sc.nextInt();
         System.out.println();
-        // Asking the user what type of phone they want
-        for (int i = 1; i <=numOfPhones; i++){
-            System.out.printf("Lets configure phone #%d\n",i);
-            System.out.print("Enter A for Android or I for iPhone: ");
-            phoneType = sc.next();
-            // Options for an iPhone
-            if (phoneType.equalsIgnoreCase("I")){
-            totalCost += iphoneCost;
-            phoneType = "IPhone";
-             } else if (phoneType.equalsIgnoreCase("A")){
-                totalCost += androidCost;
-                phoneType = "Android";
-             }
-             // Asking the screen size
-            System.out.print("What screen size? Enter 1 for 5.6\", 2 for 6.2\", or 3 for 6.7\": ");
-            int screenSize = sc.nextInt();
-            if (screenSize == 1){
-            } else if (screenSize == 2){
-                totalCost *= 1.2;
-            } else if (screenSize == 3){
-                totalCost *= 1.4;
-            }
-            // Asking the storage
-            System.out.print("How much storage? Enter 64, 128, 256: ");
-            int storage = sc.nextInt();
-            if (storage == 64){
-            } else if (storage == 128){
-                totalCost += 100;
-            } else if (storage == 256){
-                totalCost += 250;
-            }
-            // Asking the user what add-ons they want
-            System.out.println("What add-on do you want?");
-            System.out.print("   [C]ase ($49)\n   [S]creen protector ($15)\n   [E]ar buds ($99)\n   [W]ireless charger ($59)\n   [F]inish order\nEnter the letter of your choice: ");
-            String addOnChoice = sc.next();
-            if(addOnChoice.equalsIgnoreCase("C")){
-                totalCost += 49;
-            }else if(addOnChoice.equalsIgnoreCase("S")){
-                totalCost += 15;
-            }else if(addOnChoice.equalsIgnoreCase("E")){
-                totalCost += 99;
-            }else if(addOnChoice.equalsIgnoreCase("W")){
-                totalCost += 59;
-            }
-            // While loop for add-ons while the user doesn't select finish
-            while (!addOnChoice.equalsIgnoreCase("F")){
-            System.out.println("What add-on do you want?");
-            System.out.print("   [C]ase ($49)\n   [S]creen protector ($15)\n   [E]ar buds ($99)\n   [W]ireless charger ($59)\n   [F]inish order\nEnter the letter of your choice: ");
-            addOnChoice = sc.next();
-            if(addOnChoice.equalsIgnoreCase("C")){
-                totalCost += 49;
-            }else if(addOnChoice.equalsIgnoreCase("S")){
-                totalCost += 15;
-            }else if(addOnChoice.equalsIgnoreCase("E")){
-                totalCost += 99;
-            }else if(addOnChoice.equalsIgnoreCase("W")){
-                totalCost += 59;
-            }
-            }
-         }
+        phoneCalculator(numOfPhones);
          System.out.println("\nHere is a summary of your order:");
-         System.out.printf("%s\n",phoneType);
     }  
 }
