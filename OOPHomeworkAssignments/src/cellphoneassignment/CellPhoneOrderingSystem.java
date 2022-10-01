50f7014e5ed72a65e2edf8f12e2aa36c879f3ffa
Your Name <you@example.com>
2022-10-01 09:57:56 -0500

    Added header method

--- OOPHomeworkAssignments/src/cellphoneassignment/CellPhoneOrderingSystem.java
+++ OOPHomeworkAssignments/src/cellphoneassignment/CellPhoneOrderingSystem.java
@@ -1,7 +1,18 @@
 package cellphoneassignment;
-
+import java.util.Scanner;
 public class CellPhoneOrderingSystem {
+    // Method to print out the header
+    public static void header(){
+        System.out.println("****************************************************");
+        System.out.println("CORPORATE CELL PHONE ORDERING SYSTEM");
+        System.out.println("****************************************************\n");
+
+    }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Calling the header method to print out the program's header
+        header();
 
 
 
