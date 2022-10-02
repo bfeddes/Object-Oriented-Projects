9cbd03b6796504a4dd6fa36220faabab764baeae
Brian <bfeddes@hotmail.com>
2022-10-02 18:56:31 -0500

    Almost ready. Check for bugs.

--- OOPHomeworkAssignments/src/cellphoneassignment/CellPhoneOrderingSystem.java
+++ OOPHomeworkAssignments/src/cellphoneassignment/CellPhoneOrderingSystem.java
@@ -20,23 +20,31 @@ public class CellPhoneOrderingSystem {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         header();
-
         // Variables 
-        String phoneSpecs;
         String phoneType = "";
         int iphoneCost = 849;
         int androidCost = 799;
         double finalTotal = 0;
-        String totalOrder = "";
+        String orderString = "";
         String screenDimensions = "";
+        String addOnChoice;
+    
 
         // Asking the user how many phones they want to purchase
         System.out.print("How many phones will you purchase? ");
         int numOfPhones = sc.nextInt();
-        System.out.println();
-        for (int i = 0; i <numOfPhones;i++){
-        // Each phone cost.
+
+        // Loop for configuring phones
+        for (int i = 1; i <=numOfPhones;i++){
+
+        // Each phone cost and specs.
         double phoneCost = 0;
+        String allAddOns = "";
+        String addOns = "";
+
+        // Configuration number
+        System.out.printf("\nLets configure phone #%d\n",i);
+
         // Asking user for what brand of phone
         System.out.print("Enter A for Android or I for iPhone: ");
         phoneType = sc.next();
@@ -47,6 +55,7 @@ public class CellPhoneOrderingSystem {
         phoneCost = phoneCost + androidCost;
         phoneType = "Android ";
         }
+
         // Asking user for screen size
         System.out.print("What screen size? Enter 1 for 5.6\", 2 for 6.2\", or 3 for 6.7\": ");
         int screenSize = sc.nextInt();
@@ -57,23 +66,51 @@ public class CellPhoneOrderingSystem {
         phoneCost = phoneCost * 1.2;
         } else if (screenSize == 3){
         screenDimensions = "6.7\"";
-        phoneCost = phoneCost * 1.2;
+        phoneCost = phoneCost * 1.4;
         }
+
+        // Asking user for storage
         System.out.print("How much storage? Enter 64, 128, or 256: ");
         int storageAmount = sc.nextInt();
         if (storageAmount == 64){
         } else if (storageAmount == 128){
-          phoneCost = phoneCost * 1.2;
+          phoneCost = phoneCost + 100;
         } else if (storageAmount == 256){
-          phoneCost = phoneCost * 1.4;
+          phoneCost = phoneCost + 250;
         }
-        totalOrder = totalOrder + String.format("%s %s %dGB / ($%.2f) \n",phoneType, screenDimensions, storageAmount, phoneCost);
+
+        // Asking user for add ons
+        do {
+        allAddOns = allAddOns + addOns;
+        System.out.println("What add-on do you want?");
+        System.out.println("   [C]ase ($49)");
+        System.out.println("   [S]creen protector ($15)");
+        System.out.println("   [E]ar buds ($99)");
+        System.out.println("   [W]ireless charger ($59)");
+        System.out.println("   [F]inish order");
+        System.out.print("Enter the letter of your choice: ");
+        addOnChoice = sc.next();
+        if (addOnChoice.equalsIgnoreCase("C")){
+            phoneCost = phoneCost + 49;
+            addOns = " / case ";
+        } else if (addOnChoice.equalsIgnoreCase("S")){
+            phoneCost = phoneCost + 15;
+            addOns = " / screen protector";
+        } else if (addOnChoice.equalsIgnoreCase("E")){
+            phoneCost = phoneCost + 99;
+            addOns = " / ear buds";
+        } else if (addOnChoice.equalsIgnoreCase("W")){
+            phoneCost = phoneCost + 59;
+            addOns = " / wireless charger";
+        }
+        } while(!addOnChoice.equalsIgnoreCase("F"));
+        orderString = orderString + String.format("%s %s %dGB%s ($%.2f) \n",phoneType, screenDimensions, storageAmount, allAddOns, phoneCost);
         finalTotal = finalTotal + phoneCost;
       }
 
       // Printing summary
       System.out.println("\nHere is a summary of your order:");
-      System.out.println(totalOrder);
+      System.out.println(orderString);
       System.out.printf("Your total cost is $%.2f.\n",finalTotal);
       System.out.println("\nThank you for your order.");
     }
