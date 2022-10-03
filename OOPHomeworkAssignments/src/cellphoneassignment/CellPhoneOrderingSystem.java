9c526f818738f1f51e70a12cd1fd606e503a7e10
Brian <bfeddes@hotmail.com>
2022-10-02 22:27:58 -0500

    Final commit - Ready to submit

--- OOPHomeworkAssignments/src/cellphoneassignment/CellPhoneOrderingSystem.java
+++ OOPHomeworkAssignments/src/cellphoneassignment/CellPhoneOrderingSystem.java
@@ -6,114 +6,121 @@
  */
 package cellphoneassignment;
 import java.util.Scanner;
-public class CellPhoneOrderingSystem {
-    // ****Method to print out the header****
+    public class CellPhoneOrderingSystem {
+    // Method to print out the header
     public static void header(){
-        System.out.println("****************************************************");
-        System.out.println("        CORPORATE CELL PHONE ORDERING SYSTEM");
-        System.out.println("****************************************************\n");
-        System.out.println("Welcome to the coorproate cell phone ordering system.");
-        System.out.println("Here you can order multiple phones of different kinds");
-        System.out.println("with various add-ons for your employees.\n");
-        }
-    // ****Main class to organize the methods in.****
+      System.out.println("****************************************************");
+      System.out.println("        CORPORATE CELL PHONE ORDERING SYSTEM");
+      System.out.println("****************************************************\n");
+      System.out.println("Welcome to the coorproate cell phone ordering system.");
+      System.out.println("Here you can order multiple phones of different kinds");
+      System.out.println("with various add-ons for your employees.\n");
+      }
+    // Method to print out the summary
+    public static void summary(String specs, double cost){
+      // Printing summary
+      System.out.println("\nHere is a summary of your order:");
+      System.out.println(specs);
+      System.out.printf("Your total cost is $%.2f.\n",cost);
+      System.out.println("\nThank you for your order.");
+    }
+    // main class
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        header();
-        // Variables 
-        String phoneType = "";
-        int iphoneCost = 849;
-        int androidCost = 799;
-        double finalTotal = 0;
-        String orderString = "";
-        String screenDimensions = "";
-        String addOnChoice;
+      //Initiating scanner
+      Scanner sc = new Scanner(System.in);
+      // Calling header function to print out the header
+      header();
+      // Variables 
+      String phoneType = "";
+      int iphoneCost = 849;
+      int androidCost = 799;
+      double finalTotal = 0;
+      String orderString = "";
+      String screenDimensions = "";
+      String addOnChoice;
     
+      // Asking the user how many phones they want to purchase
+      System.out.print("How many phones will you purchase? ");
+      int numOfPhones = sc.nextInt();
 
-        // Asking the user how many phones they want to purchase
-        System.out.print("How many phones will you purchase? ");
-        int numOfPhones = sc.nextInt();
+      // Loop for configuring phones
+      for (int i = 1; i <=numOfPhones;i++){
 
-        // Loop for configuring phones
-        for (int i = 1; i <=numOfPhones;i++){
+      // Variables stored within the for loop so that every phone has specific specs/costs
+      double phoneCost = 0;
+      String allAddOns = "";
+      String addOns = "";
 
-        // Each phone cost and specs.
-        double phoneCost = 0;
-        String allAddOns = "";
-        String addOns = "";
+      // Configuration number
+      System.out.printf("\nLets configure phone #%d\n",i);
 
-        // Configuration number
-        System.out.printf("\nLets configure phone #%d\n",i);
+      // Asking user for what brand of phone
+      System.out.print("Enter A for Android or I for iPhone: ");
+      phoneType = sc.next();
+      if (phoneType.equalsIgnoreCase("I")){
+      phoneCost = phoneCost + iphoneCost;
+      phoneType = "IPhone ";
+      } else if (phoneType.equalsIgnoreCase("A")){
+      phoneCost = phoneCost + androidCost;
+      phoneType = "Android ";
+      }
 
-        // Asking user for what brand of phone
-        System.out.print("Enter A for Android or I for iPhone: ");
-        phoneType = sc.next();
-        if (phoneType.equalsIgnoreCase("I")){
-        phoneCost = phoneCost + iphoneCost;
-        phoneType = "IPhone ";
-        } else if (phoneType.equalsIgnoreCase("A")){
-        phoneCost = phoneCost + androidCost;
-        phoneType = "Android ";
-        }
+      // Asking user for screen size
+      System.out.print("What screen size? Enter 1 for 5.6\", 2 for 6.2\", or 3 for 6.7\": ");
+      int screenSize = sc.nextInt();
+      if (screenSize == 1){
+      screenDimensions = "5.6\"";
+      } else if (screenSize == 2){
+      screenDimensions = "6.2\"";
+      phoneCost = phoneCost * 1.2;
+      } else if (screenSize == 3){
+      screenDimensions = "6.7\"";
+      phoneCost = phoneCost * 1.4;
+      }
 
-        // Asking user for screen size
-        System.out.print("What screen size? Enter 1 for 5.6\", 2 for 6.2\", or 3 for 6.7\": ");
-        int screenSize = sc.nextInt();
-        if (screenSize == 1){
-         screenDimensions = "5.6\"";
-        } else if (screenSize == 2){
-        screenDimensions = "6.2\"";
-        phoneCost = phoneCost * 1.2;
-        } else if (screenSize == 3){
-        screenDimensions = "6.7\"";
-        phoneCost = phoneCost * 1.4;
-        }
+      // Asking user for storage
+      System.out.print("How much storage? Enter 64, 128, or 256: ");
+      int storageAmount = sc.nextInt();
 
-        // Asking user for storage
-        System.out.print("How much storage? Enter 64, 128, or 256: ");
-        int storageAmount = sc.nextInt();
-        if (storageAmount == 64){
-        } else if (storageAmount == 128){
-          phoneCost = phoneCost + 100;
-        } else if (storageAmount == 256){
-          phoneCost = phoneCost + 250;
-        }
+      if (storageAmount == 64){
+      } else if (storageAmount == 128){
+      phoneCost = phoneCost + 100;
+      } else if (storageAmount == 256){
+      phoneCost = phoneCost + 250;
+      } else {System.out.println("You did not enter a valid amount of storage.");}
 
-        // Asking user for add ons
-        do {
-        allAddOns = allAddOns + addOns;
-        System.out.println("What add-on do you want?");
-        System.out.println("   [C]ase ($49)");
-        System.out.println("   [S]creen protector ($15)");
-        System.out.println("   [E]ar buds ($99)");
-        System.out.println("   [W]ireless charger ($59)");
-        System.out.println("   [F]inish order");
-        System.out.print("Enter the letter of your choice: ");
-        addOnChoice = sc.next();
-        if (addOnChoice.equalsIgnoreCase("C")){
-            phoneCost = phoneCost + 49;
-            addOns = " / case ";
+      // Asking user for add ons
+      do {
+      allAddOns = allAddOns + addOns;
+      System.out.println("What add-on do you want?");
+      System.out.println("   [C]ase ($49)");
+      System.out.println("   [S]creen protector ($15)");
+      System.out.println("   [E]ar buds ($99)");
+      System.out.println("   [W]ireless charger ($59)");
+      System.out.println("   [F]inish order");
+      System.out.print("Enter the letter of your choice: ");
+      addOnChoice = sc.next();
+      if (addOnChoice.equalsIgnoreCase("C")){
+        phoneCost = phoneCost + 49;
+        addOns = " / case ";
         } else if (addOnChoice.equalsIgnoreCase("S")){
-            phoneCost = phoneCost + 15;
-            addOns = " / screen protector";
+          phoneCost = phoneCost + 15;
+          addOns = " / screen protector";
         } else if (addOnChoice.equalsIgnoreCase("E")){
-            phoneCost = phoneCost + 99;
-            addOns = " / ear buds";
+          phoneCost = phoneCost + 99;
+          addOns = " / ear buds";
         } else if (addOnChoice.equalsIgnoreCase("W")){
-            phoneCost = phoneCost + 59;
-            addOns = " / wireless charger";
+          phoneCost = phoneCost + 59;
+          addOns = " / wireless charger";
         }
         } while(!addOnChoice.equalsIgnoreCase("F"));
 
-        // Storing the results
-        orderString = orderString + String.format("%s %s %dGB%s ($%.2f) \n",phoneType, screenDimensions, storageAmount, allAddOns, phoneCost);
-        finalTotal = finalTotal + phoneCost;
+      // Storing the results
+      orderString = orderString + String.format("%s %s %dGB%s ($%.2f) \n",phoneType, screenDimensions, storageAmount, allAddOns, phoneCost);
+      finalTotal = finalTotal + phoneCost;
       }
 
-      // Printing summary
-      System.out.println("\nHere is a summary of your order:");
-      System.out.println(orderString);
-      System.out.printf("Your total cost is $%.2f.\n",finalTotal);
-      System.out.println("\nThank you for your order.");
+      // Calling the summary method
+      summary(orderString, finalTotal);
     }
   }
