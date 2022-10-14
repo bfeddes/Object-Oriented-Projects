9c2beba7ef9bd6f1d8ee3a0a53e2e5cf9ab48b71
Brian <bfeddes@hotmail.com>
2022-10-14 00:13:48 -0500

    Created a testing file

---
+++ OOPHomeworkAssignments/src/testingarea/test.java
@@ -0,0 +1,10 @@
+package testingarea;
+
+public class test {
+    public static void main(String[] args){
+        System.out.println("***************************************************");
+        System.out.println("\t\t2022 WNBA STANDINGS\n");
+        System.out.println("***************************************************");
+    }
+    
+}
