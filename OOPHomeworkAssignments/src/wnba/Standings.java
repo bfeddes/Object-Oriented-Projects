c14dc22bffb4116cde5829623c5d100295a2ab30
Brian <bfeddes@hotmail.com>
2022-10-14 12:58:44 -0500

    Put everything into a method. Need to seperate.

--- OOPHomeworkAssignments/src/wnba/Standings.java
+++ OOPHomeworkAssignments/src/wnba/Standings.java
@@ -2,6 +2,29 @@ package wnba;
 import java.util.Scanner;
 import java.io.File;
 public class Standings {
+    public static void easternConference(String fileName){
+        // Variables
+        String line;
+        String[] parts;
+        String[] eastern = new String[6];
+        String[] target;
+        String teamName = "";
+        double wins, losses, winningPct, gamesBack;
+        try {
+            // Initializing Scanner object for the user to select a file path
+            Scanner fsc = new Scanner(new File(fileName));
+            while(fsc.hasNextLine()){
+                line = fsc.nextLine();
+                if (line.length() > 0){
+                    parts = line.split("\t");
+                    teamName = parts[0]; 
+                }
+                System.out.println(teamName);
+            }
+        } catch (Exception ex) {
+            System.out.println("The file you entered is invalid");
+        }
+}
     public static void header(){
         System.out.println("***************************************************");
         System.out.println("\t       2022 WNBA STANDINGS");
@@ -16,36 +39,8 @@ public class Standings {
     public static void main(String[] args){
         // Initializing Scanner object for user input
         Scanner sc = new Scanner(System.in);
-
-        // Variables
-        String fileName;
-        String line;
-        String teamName;
-        String[] parts;
-        String[] eastern = new String[6];
-        String[] western = new String[6];
-        int wins, losses;
-        double winningPct, gamesBack;
-
-        // Calling header function to print the program header
-        header();
-        // Asking the user to enter the file name
-        System.out.print("Enter the standings filename: ");
-        fileName = sc.nextLine();
-
-        // Initializing Scanner object for the user to select a file path
-        try {
-            Scanner fsc = new Scanner(new File(fileName));
-            while(fsc.hasNextLine()){
-                line = fsc.nextLine();
-                if (line.length() > 0){
-                parts = line.split("\t");
-                teamName = parts[0]; 
-                System.out.println(teamName);
-                }
-            }
-        } catch (Exception ex) {
-            System.out.println("The file you entered is invalid");
-        }
+        System.out.print("Enter name of file: ");
+        String fileName = sc.nextLine();
+        easternConference(fileName);
     }
 }
