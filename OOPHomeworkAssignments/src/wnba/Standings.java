cb1dabb1b4ed95ac3a67894fc202c33b054da101
Brian <bfeddes@hotmail.com>
2022-10-14 11:58:14 -0500

    Prints out both conferences.

--- OOPHomeworkAssignments/src/wnba/Standings.java
+++ OOPHomeworkAssignments/src/wnba/Standings.java
@@ -11,7 +11,7 @@ public class Standings {
      * Offer the user options to see eastern conference standings
      * Offer the user options to see western conference standings
      * Offer the user options to see overall standings
-     * Each display of standings will show the team name, number of wins, number of losses, winning percentage, and games behind. Teams sorted by winning percentage
+     * Each display of standings will show the team name, number of wins, number of losses, winning percentage, and games behind, teams sorted by winning percentage
      */
     public static void main(String[] args){
         // Initializing Scanner object for user input
