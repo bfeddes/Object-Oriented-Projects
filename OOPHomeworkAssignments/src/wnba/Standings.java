6f087eb06dc105a31868265616af8f59d93bf6b3
Brian <bfeddes@hotmail.com>
2022-10-13 23:57:55 -0500

    Wrote to-do list, imported objects

---
+++ OOPHomeworkAssignments/src/wnba/Standings.java
@@ -0,0 +1,16 @@
+package wnba;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+public class Standings {
+    /* Offer the user options to see eastern conference standings
+     * Offer the user options to see western conference standings
+     * Offer the user options to see overall standings
+     * Each display of standings will show the team name, number of wins, number of losses, winning percentage, and games behind. Teams sorted by winning percentage
+     */
+    public static void main(String[] args){
+
+    }
+}
