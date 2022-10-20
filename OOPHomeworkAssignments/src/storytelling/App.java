ac699ca867327713682678ca9a7841dc83d99aa1
Brian <bfeddes@hotmail.com>
2022-10-20 18:40:14 -0500

    Created and implemented header method.

--- OOPHomeworkAssignments/src/storytelling/App.java
+++ OOPHomeworkAssignments/src/storytelling/App.java
@@ -1,8 +1,27 @@
+/* Brian Feddes
+ * StoryTelling Programming Assignment
+ * Object Oriented Programming
+ * Professor Klump
+ * Fall 2022
+ */
 package storytelling;
-
+import java.util.Scanner;
 public class App {
-    public static void main(String[] args){
-        
+    // Method for printing out the program's header
+    public static void header(){
+        System.out.println("****************************************************");
+        System.out.println("\t\t  STORYTELLER V1.0");
+        System.out.println("****************************************************");
+        System.out.println("\nWelcome to StoryTeller, a sophisticated electronic\nauthor. This program reads from a list of words of\nvarious parts of speech and creates a story from\nthem. You can tune the richness of the writing by\nchanging how frequently adjectives, adverbs, and\nprepositions should be included.\n");
     }
-    
+    public static void main(String[] args){
+        // Initializing Scanner object
+        Scanner sc = new Scanner(System.in);
+
+        // Calling the header method
+        header();
+
+
+
+    }
 }
