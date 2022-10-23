3530bb43c9039ec8e4c64d37e516273711c5549b
Your Name <you@example.com>
2022-10-23 15:49:51 -0500

    To-do list added + imports

--- OOPHomeworkAssignments/src/storytelling/Author.java
+++ OOPHomeworkAssignments/src/storytelling/Author.java
@@ -1,5 +1,13 @@
 package storytelling;
-
+import java.util.Scanner;
+import java.util.LinkedHashMap;
+import java.util.ArrayList;
+import java.util.Random;
+/* TO DO FOR AUTHOR
+ * How frequenty should adjectives be used
+ * How frequently should adverbs be used
+ * How frequently should prepositions be used? (If we're using them)
+ */
 public class Author {
     
 }
