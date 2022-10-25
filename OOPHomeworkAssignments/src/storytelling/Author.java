6c6f14f7dbef40d1cf364773f5a1ab2148b374a7
Brian <bfeddes@hotmail.com>
2022-10-25 18:34:17 -0500

    Imported stuff + Variables

--- OOPHomeworkAssignments/src/storytelling/Author.java
+++ OOPHomeworkAssignments/src/storytelling/Author.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Random;
 public class Author {
-    // Add a LinkedHashMap to house the collection of words it can use from WordFileReader.
+    // Add a LinkedHashMap to house the collection of words it can use from WordFileReader
     private int adjFrequency;
     private int advFequency;
     private int prepFrequency;
