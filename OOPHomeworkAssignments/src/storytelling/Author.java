786c4db50b441fff0d4421fe9ca1324a153c352d
Brian <bfeddes@hotmail.com>
2022-10-25 18:31:35 -0500

    Imported stuff I will need + variables

--- OOPHomeworkAssignments/src/storytelling/Author.java
+++ OOPHomeworkAssignments/src/storytelling/Author.java
@@ -1,5 +1,12 @@
 package storytelling;
-
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Random;
 public class Author {
+    // Add a LinkedHashMap to house the collection of words it can use from WordFileReader.
+    private int adjFrequency;
+    private int advFequency;
+    private int prepFrequency;
+
     
 }
