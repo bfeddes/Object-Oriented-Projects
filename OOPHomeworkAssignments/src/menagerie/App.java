65ec05a96b6002217c892fe2bc07565e4b52b69e
Brian <bfeddes@hotmail.com>
2022-11-02 16:31:18 -0500

    Initial commit. Created different classes

---
+++ OOPHomeworkAssignments/src/menagerie/App.java
@@ -0,0 +1,16 @@
+/* Brian Feddes
+ * Professor Klump
+ * Object-Oriented Programming
+ * Fall 2022
+ * Menagerie Assignment, App Class
+ */
+package menagerie;
+public class App {
+    public static void header(){
+        System.out.println();
+    }
+    public static void main(String[] args){
+
+    }
+    
+}
