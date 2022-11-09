bad2ffd66b8d2ccff4e490acab6f47ee2676a88a
Brian <bfeddes@hotmail.com>
2022-11-08 22:58:17 -0600

    Need to check on the cutoff values

--- OOPHomeworkAssignments/src/menagerie/Pet.java
+++ OOPHomeworkAssignments/src/menagerie/Pet.java
@@ -74,21 +74,21 @@ public abstract class Pet implements Comparable<Pet> {
     // Functions to determine if the pet is sleeping, needs food, or needs attention
     public boolean needsFood() {
         if (getEatProb() > rnd.nextInt(24)) {
-        return true;
+            return true;
         } else {
             return false;
         }
     }
     public boolean needsSleep() {
         if (getSleepProb() > rnd.nextInt(24)) {
-        return true;
+            return true;
         } else {
             return false;
         }
     }
     public boolean needsAtt() {
         if (getSeekAttProb() > rnd.nextInt(24)) {
-        return true;
+            return true;
         } else {
             return false;
         }
