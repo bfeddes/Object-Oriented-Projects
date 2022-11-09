c3623ad705f7a2bf9d973b1f1c2a403ddaafdcc3
Brian <bfeddes@hotmail.com>
2022-11-08 22:58:42 -0600

    Need to work on act() class

--- OOPHomeworkAssignments/src/menagerie/Dog.java
+++ OOPHomeworkAssignments/src/menagerie/Dog.java
@@ -5,28 +5,101 @@
  * Menagerie Assignment, Dog Class
  */
 package menagerie;
+import java.util.Random;
 public class Dog extends Pet {
+    private int biteProb;
+    private int comfortedHuman;
+    private int chewedUpShoe;
+    private int ateHomework;
+    private Random rnd;
 
+    public int getBiteProb() {
+        return biteProb;
+    }
+    public void setBiteProb(int biteProb) {
+        this.biteProb = biteProb;
+    }
+    public int getComfortedHuman() {
+        return comfortedHuman;
+    }
+    public void setComfortedHuman(int comfortedHuman) {
+        this.comfortedHuman = comfortedHuman;
+    }
+    public int getChewedUpShoe() {
+        return chewedUpShoe;
+    }
+    public void setChewedUpShoe(int chewedUpShoe) {
+        this.chewedUpShoe = chewedUpShoe;
+    }
+    public int getAteHomework() {
+        return ateHomework;
+    }
+    public void setAteHomework(int ateHomework) {
+        this.ateHomework = ateHomework;
+    }
     // Constructors
     public Dog(){
         super();
     }
-    public Dog(String name, int age, double weight){
+    public Dog(String name, int age, double weight) {
         super(name, age, weight);
     }
-    // Review this
-    public String foodMessage(){
-        if (needsFood()){
-            return getName() + "Grabs a biscuit";
+    public boolean ateHomework() {
+        if (getAteHomework() > rnd.nextInt(24)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public boolean chewedUpShoe() {
+        if (getChewedUpShoe() > rnd.nextInt(24)) {
+            return true;
         } else {
-            return "";
+            return false;
+        }
+    }
+    public boolean comfortedHuman() {
+        if (getComfortedHuman() > rnd.nextInt(24)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean biteHuman() {
+        if (getBiteProb() > rnd.nextInt(24)) {
+            return true;
+        } else {
+            return false;
         }
     }
     @Override
     public void animalCutoffs(){
-        setEatProb(3);
-        setSleepProb(10);
-        setSeekAttProb(16);
+        setEatProb(24);
+        setSleepProb(24);
+        setSeekAttProb(24);
+        setAteHomework(9);
+        setChewedUpShoe(5);
+        setComfortedHuman(10);
+        setBiteProb(1); 
+    }
+    @Override 
+    public String act() {
+        String extraActivities = "";
+        extraActivities = extraActivities.concat(super.act());
+        if (chewedUpShoe()) { 
+            extraActivities = extraActivities.concat("The " + getAnimalType() + ", " + getName() + " was bored and chewed up your new shoes.\n");
+        }
+        if (ateHomework()) {
+            extraActivities = extraActivities.concat("The " + getAnimalType() + ", " + getName() + " Ate your homework the night before it was due.\n");
+        }
+        if (comfortedHuman()) {
+            extraActivities = extraActivities.concat("The " + getAnimalType() + ", " + getName() + " Saw you were sad and comforted you.\n");
+        }
+        if (biteHuman()) {
+            extraActivities = extraActivities.concat("The " + getAnimalType() + ", " + getName() + " Playfully bit you.\n");
+        }
+        return extraActivities;
     }
     @Override
     public String getAnimalType() {
