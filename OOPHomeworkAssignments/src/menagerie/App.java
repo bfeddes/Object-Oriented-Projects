37a07ba995a3d1e7e9db1c1ef168fe38f0d9cc2b
Brian <bfeddes@hotmail.com>
2022-11-09 17:27:12 -0600

    App - Done

--- OOPHomeworkAssignments/src/menagerie/App.java
+++ OOPHomeworkAssignments/src/menagerie/App.java
@@ -18,7 +18,7 @@ public class App {
         Scanner sc = new Scanner(System.in);
         // Variables
         int userChoice;
-        String petChoice;
+        String petChoice, fileName;
         ArrayList<Pet> pets = new ArrayList<Pet>();
         // Calling the header method
         header();
@@ -26,7 +26,7 @@ public class App {
         // Asking the user what they'd like to do while number choice is not 7. (Skeleton code right now)
         do{
             System.out.println("\nWhat would you like to do?");
-            System.out.println("1. Add a new pet."); // Will then ask the user to choose a pet type, and then name, age, and weight
+            System.out.println("1. Add a new pet.");
             System.out.println("2. Print list of pets.");
             System.out.println("3. Save pets to a file.");
             System.out.println("4. Load pets from a file.");
@@ -42,6 +42,9 @@ public class App {
             if (userChoice == 1){
                 System.out.print("Enter d for dog, c for cat, or f for fish: ");
                 petChoice = sc.next();
+                if (!petChoice.equalsIgnoreCase("d") && !petChoice.equalsIgnoreCase("c") && !petChoice.equalsIgnoreCase("f")){
+                    System.out.println("Invalid pet type.");
+                } else {
                 System.out.print("Enter name, age, and weight: ");
                 String petName = sc.next();
                 int petAge = sc.nextInt();
@@ -50,20 +53,43 @@ public class App {
                     pets.add(new Dog(petName, petAge, petWeight));
                 } else if (petChoice.equalsIgnoreCase("c")) {
                     pets.add(new Cat(petName, petAge, petWeight));
+                } else if (petChoice.equalsIgnoreCase("f")) {
+                    pets.add(new Fish(petName, petAge, petWeight));
                 }
                 Collections.sort(pets);
+                }
             }
             // This option will print out the list of pets in alphabetical order by name
             if (userChoice == 2) {
-                if (pets.isEmpty()){
-                    System.out.println("\nThe list of pets is empty.");
-                } else {
-                    System.out.println("Here is your list of pets:");
-                    for (Pet pet : pets) {
-                        System.out.println(pet);
-                    }
+                System.out.println("\nHere is your list of pets:");
+                for (Pet pet : pets) {
+                    System.out.println(pet);
                 }
             }
+            // This option will allow the user to save their list of pets to a file.
+            if (userChoice == 3) {
+                System.out.print("Enter name of file to save: ");
+                sc.nextLine();
+                fileName = sc.nextLine();
+                if (PetWriter.writeToFile(pets, fileName)) {
+                    System.out.println("The pets were saved to the file.");
+                } else {
+                    System.out.println("The pets were unable to be saved to the file.");
+                }
+            }
+            // This option will allow users to load a list of pets from a file.
+            if (userChoice == 4) {
+                try{
+                System.out.print("\nEnter name of file to load: ");
+                sc.nextLine();
+                fileName = sc.nextLine();
+                pets = PetReader.readPetsFromText(fileName);
+                System.out.println("The pets were read from the file.");
+                } catch (Exception ex) {
+                    System.out.println("The pets were unable to be read from the file.");
+                }
+            }
+            // This option will simulate a day in the life of all pets in the ArrayList of pets.
             if (userChoice == 5) {
                 if (pets.isEmpty()){
                     System.out.println("There are no pets in your list.");
@@ -77,7 +103,7 @@ public class App {
                     }
                 }
             }
-            // This choice will clear the ArrayList of pets
+            // This option will clear the ArrayList of pets
             if (userChoice == 6){
                 pets.clear();
             }
@@ -86,5 +112,4 @@ public class App {
         // Goodbye message
         System.out.println("\nThank you for using Menagerie. We hope you had fun.");
     }
-    
 }
