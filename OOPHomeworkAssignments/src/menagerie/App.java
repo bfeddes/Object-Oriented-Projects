b80f5bbcf5aaa4bec84d321f44cc0a5ebadac09b
Brian <bfeddes@hotmail.com>
2022-11-08 18:31:02 -0600

    Added options 1, 2, and 6.

--- OOPHomeworkAssignments/src/menagerie/App.java
+++ OOPHomeworkAssignments/src/menagerie/App.java
@@ -12,17 +12,20 @@ public class App {
     public static void header(){
         System.out.println("************************************************************");
         System.out.println("\t      Menagerie V1.0: The Pet Simulator");
-        System.out.println("************************************************************\n");
+        System.out.println("************************************************************");
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         // Variables
         int userChoice;
+        String petChoice;
+        ArrayList<Pet> pets = new ArrayList<Pet>();
         // Calling the header method
         header();
+        
         // Asking the user what they'd like to do while number choice is not 7. (Skeleton code right now)
         do{
-            System.out.println("What would you like to do?");
+            System.out.println("\nWhat would you like to do?");
             System.out.println("1. Add a new pet."); // Will then ask the user to choose a pet type, and then name, age, and weight
             System.out.println("2. Print list of pets.");
             System.out.println("3. Save pets to a file.");
@@ -34,6 +37,35 @@ public class App {
             userChoice = sc.nextInt();
             if (userChoice < 1 || userChoice > 7){
                 System.out.println("Invalid selection.");
+            }
+            // This option allows a user to add a pet to the ArrayList "pets"
+            if (userChoice == 1){
+                System.out.print("Enter d for dog, c for cat, or f for fish: ");
+                petChoice = sc.next();
+                System.out.print("Enter name, age, and weight: ");
+                String petName = sc.next();
+                int petAge = sc.nextInt();
+                double petWeight = sc.nextDouble();
+                if (petChoice.equalsIgnoreCase("d")){
+                    pets.add(new Dog(petName, petAge, petWeight));
+                }
+                Collections.sort(pets);
+            }
+            // This option will print out the list of pets in alphabetical order by name
+            if (userChoice == 2) {
+                for (Pet pet : pets) {
+                    System.out.println(pet);
+                }
+                if (pets.isEmpty()){
+                    System.out.println("\nThe list of pets is empty.");
+                }
+            }
+
+
+
+            // This choice will clear the ArrayList of pets
+            if (userChoice == 6){
+                pets.clear();
             }
         } while (userChoice != 7);
 
