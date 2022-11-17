f6552fec671daf2ba5fe5ba27d82d2371c52b5a8
Your Name <you@example.com>
2022-11-16 21:47:39 -0600

    Added comments to WrittenArtisticWork

--- OOPHomeworkAssignments/src/muse/WrittenArtisticWork.java
+++ OOPHomeworkAssignments/src/muse/WrittenArtisticWork.java
@@ -1,10 +1,11 @@
 package muse;
 
 import java.util.LinkedHashMap;
-
 public abstract class WrittenArtisticWork extends ArtisticWork {
+    // Variables
     private String language;
     private String text;  // the actual content
+    // Getters and setters
     public String getLanguage() {
         return language;
     }
@@ -17,25 +18,30 @@ public abstract class WrittenArtisticWork extends ArtisticWork {
     public void setText(String text) {
         this.text = text;
     }
+    // Default constructor
     public WrittenArtisticWork() {
         language = "English";
         text = "This is the text of the written work.";
     }
+    // Non-default constructor
     public WrittenArtisticWork(String creator, String date, String title, String description,
             String language, String text) {
         super(creator,date,title,description);
         setLanguage(language);
         setText(text);
     }
+    // Non-default constructor
     public WrittenArtisticWork(LinkedHashMap<String,String> settings) {
         super(settings);
         setLanguage(settings.get("language"));
         setText(settings.get("text"));
     }
+    // Overriding general info string from the super class
     @Override
     public String getGeneralInfoString() {
         return String.format("%s\nLanguage: %s",super.getGeneralInfoString(),language);
     }
+    // Overriding the general info string from the super class
     @Override
     public String getSpecificInfoString() {
         return text;
