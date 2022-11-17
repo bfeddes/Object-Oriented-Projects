18c69f0fa174bd8c805e7e218bb6a1079d22e9d3
Your Name <you@example.com>
2022-11-16 21:41:01 -0600

    Added comments for Poem

--- OOPHomeworkAssignments/src/muse/Poem.java
+++ OOPHomeworkAssignments/src/muse/Poem.java
@@ -1,31 +1,37 @@
 package muse;
 
 import java.util.LinkedHashMap;
-
 public class Poem extends WrittenArtisticWork {
+    // Variable
     private String meter;
+    //Getters and setters
     public String getMeter() {
         return meter;
     }
     public void setMeter(String meter) {
         this.meter = meter;
     }
+    // Default constructor
     public Poem() {
         meter = "N/A";
     }
+    // Non-default constructor
     public Poem(String creator, String date, String title, String description,
             String language, String text, String meter) {
         super(creator,date,title,description,language,text);
         setMeter(meter);
     }
+    // Non-default constructor
     public Poem(LinkedHashMap<String,String> settings) {
         super(settings);
         setMeter(settings.get("meter"));
     }
+    // Overriding the getType method
     @Override
     public String getType() {
         return "poem";
     }
+    // Overriding the specific info for poems
     @Override
     public String getSpecificInfoString() {
         return String.format("Meter: %s\n%s", meter, super.getSpecificInfoString());
