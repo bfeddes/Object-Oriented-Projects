a44335c8cd88b4fbb82bb89804b5d82644cc7a2c
Your Name <you@example.com>
2022-11-16 21:42:24 -0600

    Added comments to RecordedArtisticWork

--- OOPHomeworkAssignments/src/muse/RecordedArtisticWork.java
+++ OOPHomeworkAssignments/src/muse/RecordedArtisticWork.java
@@ -1,9 +1,11 @@
 package muse;
 import java.util.LinkedHashMap;
 public abstract class RecordedArtisticWork extends ArtisticWork {
+    // Variables
     private int duration; // in seconds
     private String fileName;
     private double fileSize;
+    // Getters and setters
     public int getDuration() {
         return duration;
     }
@@ -30,11 +32,13 @@ public abstract class RecordedArtisticWork extends ArtisticWork {
             fileSize = fsize;
         }
     }
+    // Default constructor
     public RecordedArtisticWork() {
         fileName = "";
         fileSize = 0;
         duration = 0;
     }
+    // Non-default constructor
     public RecordedArtisticWork(String creator, String date, String title, String description,
             int duration, String fileName, double fileSize) {
         super(creator,date,title,description);
@@ -42,12 +46,14 @@ public abstract class RecordedArtisticWork extends ArtisticWork {
         setFileName(fileName);
         setFileSize(fileSize);
     }
+    // Non-default constructor
     public RecordedArtisticWork(LinkedHashMap<String,String> settings) {
         super(settings);
         setDuration(Integer.parseInt(settings.get("duration")));
         setFileName(settings.get("filename"));
         setFileSize(Double.parseDouble(settings.get("filesize")));
     }
+    // Overriding the information
     @Override
     public String getGeneralInfoString() {
         return String.format("%s\nDuration: %d sec; Filename: %s (%.2f MB)", super.getGeneralInfoString(),
