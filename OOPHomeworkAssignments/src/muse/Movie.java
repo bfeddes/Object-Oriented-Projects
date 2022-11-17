93b0f5e4eec7808a280923c5c0b931797b7bf622
Your Name <you@example.com>
2022-11-16 21:39:38 -0600

    Added comments to Movie

--- OOPHomeworkAssignments/src/muse/Movie.java
+++ OOPHomeworkAssignments/src/muse/Movie.java
@@ -1,8 +1,10 @@
 package muse;
 import java.util.LinkedHashMap;
 public class Movie extends RecordedArtisticWork {
+    // Variables
     private int frameRate; // frames per second
     private String resolution;  // 720p, 1040p, 2048p, etc
+    // Getters and setters
     public int getFrameRate() {
         return frameRate;
     }
@@ -19,25 +21,30 @@ public class Movie extends RecordedArtisticWork {
     public void setResolution(String res) {
         resolution = res;
     }
+    // Default constructor
     public Movie() {
         frameRate = 30;
         resolution = "720p";
     }
+    // Non-default constructor
     public Movie(String creator, String date, String title, String description,
             int duration, String fileName, double fileSize, int frameRate, String resolution) {
         super(creator,date,title,description,duration,fileName,fileSize);
         setFrameRate(frameRate);
         setResolution(resolution);
     }
+    // Non-default constructor
     public Movie(LinkedHashMap<String,String> settings) {
        super(settings);
        setFrameRate(Integer.parseInt(settings.get("framerate")));
        setResolution(settings.get("resolution"));
     }
+    // Overriding the getType method 
     @Override
     public String getType() {
         return "movie";
     }
+    // Overriding the specific info 
     @Override
     public String getSpecificInfoString() {
         return String.format("filmed with %s resolution at %d frames per second", resolution, frameRate);
