529c8ea66f4e67361a5453738c72fa7f7b791864
Your Name <you@example.com>
2022-11-21 15:26:33 -0600

    Added Binary option. Need to test

--- OOPHomeworkAssignments/src/muse/ArtisticWorkWriter.java
+++ OOPHomeworkAssignments/src/muse/ArtisticWorkWriter.java
@@ -1,11 +1,25 @@
 package muse;
 
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.io.File;
 public class ArtisticWorkWriter {
   // Text writer
   
   // XML Writer
   
-  // Binary Writer
+  public static boolean writeToBinary(ArrayList<ArtisticWork> works, String fname) {
+    try {
+        FileOutputStream fos = new FileOutputStream(new File(fname));
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(works);
+        oos.close();
+        return true;
+    } catch (Exception ex) {
+        return false;
+    }
+}
   
   // Potentially JSON Writer
   
