6baa69b14201eb146719714b56c9766ab9f8506f
Brian <bfeddes@hotmail.com>
2022-11-22 09:25:41 -0600

    Added reading from Binary

--- OOPHomeworkAssignments/src/muse/ArtisticWorkReader.java
+++ OOPHomeworkAssignments/src/muse/ArtisticWorkReader.java
@@ -1,12 +1,27 @@
 package muse;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+@SuppressWarnings("unchecked") // I got annoyed by the warning that casting it as an ArrayList of ArtisticWorks give, so I added this in.
 public class ArtisticWorkReader {
-  // Text Reader
-  
-  // XML Reader
-  
-  // Binary Reader
-  
-  // Potentially JSON Reader
-  
+
+    // CODE FOR XML
+
+    // CODE FOR TEXT
+
+    // Method for reading a binary file
+    public static ArrayList<ArtisticWork> readFromBinary (String fname) {
+        try {
+            FileInputStream fileInput = new FileInputStream(new File(fname));
+            ObjectInputStream objectInput = new ObjectInputStream(fileInput);
+            ArrayList<ArtisticWork> artWorks = (ArrayList<ArtisticWork>)objectInput.readObject();
+            objectInput.close();
+            return artWorks;
+        } catch (Exception ex) {
+            return null;
+        }
+      }
+    // POTENTIAL CODE FOR JSON
 }
