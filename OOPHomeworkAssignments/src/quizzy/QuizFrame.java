7305dec56259c1215f8b3c94c1918e8bafeebacc
Brian <bfeddes@hotmail.com>
2022-12-03 11:24:36 -0600

    Work on loading in files clearing welcome msg

--- OOPHomeworkAssignments/src/quizzy/QuizFrame.java
+++ OOPHomeworkAssignments/src/quizzy/QuizFrame.java
@@ -1,13 +1,23 @@
 package quizzy;
 
+import java.awt.Container;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import javax.swing.JOptionPane;
+import java.awt.Graphics;
 
 public class QuizFrame extends JFrame{
     // Variables 
-    Question question;
+    private Question question;
 
     // setupMenu method 
     public void setupMenu() {
@@ -20,19 +30,48 @@ public class QuizFrame extends JFrame{
         menuBar.add(menuQuiz);
         // Creating and adding the file and quiz menu items 
         JMenuItem fileLoad = new JMenuItem("Load questions");
-        JMenuItem fileExit = new JMenuItem("Exit");
+        JMenuItem exitApp = new JMenuItem("Exit");
         JMenuItem quizStart = new JMenuItem("Start");
         JMenuItem quizStop = new JMenuItem("Stop");
-        menuFile.add(fileLoad);
-        menuFile.add(fileExit);
+        menuFile.add(exitApp);
         menuQuiz.add(quizStart);
         menuQuiz.add(quizStop);
+        exitApp.addActionListener(
+            new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    System.exit(0);
+                }
+            }
+        );
+        menuFile.add(fileLoad);
+        fileLoad.addActionListener(
+            new ActionListener() {
+                public void actionPerformed (ActionEvent e) {
+                    JFileChooser fileChoice = new JFileChooser(); 
+                    File fileName;
+                    if (fileChoice.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                        fileName = fileChoice.getSelectedFile();
+                        if (QuestionsReader.readFromTextFile(fileName, question) != null) {
+                            repaint();
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Could not read question.");
+                        }
+                    }
+                }
+            }
+        );
+        menuFile.add(fileLoad);
     }
     // setupGUI method 
     public void setupGUI() {
         setTitle("Object-Oriented Quiz Tool");
         setBounds(100,100,850,275);
         setupMenu();
+        Container c = getContentPane();
+        c.setLayout(new BorderLayout());
+        JTextArea welcomeMessage = new JTextArea("Welcome to Quizzy, the object-oriented programming quiz tool.\nSelect File >> Load Questions to begin.");
+        welcomeMessage.setEditable(false);
+        c.add(welcomeMessage);
     }
     // Constructor
     public QuizFrame(Question question) {
