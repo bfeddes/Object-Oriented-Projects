424741bd538f3906ed7113b8320ec200c017e74f
Brian <bfeddes@hotmail.com>
2022-12-12 23:14:59 -0600

    Added multiple options. Little functionality

--- OOPHomeworkAssignments/src/createaplayer/PlayerFrame.java
+++ OOPHomeworkAssignments/src/createaplayer/PlayerFrame.java
@@ -2,6 +2,10 @@ package createaplayer;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
@@ -12,6 +16,8 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
 import java.util.ArrayList;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PlayerFrame extends JFrame {
     // Variables
@@ -21,23 +27,53 @@ public class PlayerFrame extends JFrame {
     private JButton btnNext;
     private Player player;
     private ArrayList myPlayers;
-    private JTextArea screenMessage;
+    private JLabel screenMessage;
     // setupMenu method 
     public void setupMenu() {
-
+        JMenuBar mbar = new JMenuBar();
+        setJMenuBar(mbar);
+        JMenu mnuFile = new JMenu("File");
+        JMenu mnuMyPlayer = new JMenu("MyPlayer");
+        mbar.add(mnuFile);
+        mbar.add(mnuMyPlayer);
+        JMenuItem mnuSave = new JMenuItem("Save MyPlayers");
+        JMenuItem mnuLoad = new JMenuItem("Load Players");
+        JMenuItem mnuExit = new JMenuItem("Exit");
+        mnuFile.add(mnuSave);
+        mnuFile.add(mnuLoad);
+        mnuFile.add(mnuExit);
+        JMenuItem mnuCreatePlayer = new JMenuItem("Create a MyPlayer");
+        JMenuItem mnuDeletePlayer = new JMenuItem("Delete a MyPlayer");
+        mnuMyPlayer.add(mnuCreatePlayer);
+        mnuMyPlayer.add(mnuDeletePlayer);
+        mnuExit.addActionListener(
+            new ActionListener() {
+                public void actionPerformed (ActionEvent e) {
+                    JOptionPane.showMessageDialog(null, "We hope you enjoyed!");
+                    System.exit(0);
+                }
+            }
+        );
+        
+        // Checking if there are any players to delete, if not, delete option will not be enabled 
+        if (myPlayers == null) {
+            mnuDeletePlayer.setEnabled(false);
+        } else {
+            mnuDeletePlayer.setEnabled(true);
+        }
     }
 
     // setupGUI method
     public void setupGUI() {
+        setupMenu();
         setTitle("MyPlayer Creator");
         setBounds(250, 100, 800, 500);
         Container c = getContentPane();
         c.setBackground(Color.DARK_GRAY);
         c.setLayout(new BorderLayout());
-        screenMessage = new JTextArea("Welcome to MyPlayer creator. Please select one of the options below to select your sport.");
-        screenMessage.setSize(500, 100);
-        screenMessage.setEditable(false);
-        screenMessage.setFont(new Font("Impact", Font.PLAIN,15));
+        screenMessage = new JLabel("<html> Welcome to MyPlayer creator!<br/>Please select the sport you would like to create a player in from the options below</html>");
+        screenMessage.setFont(new Font("Elephant", Font.PLAIN, 16));
+        screenMessage.setForeground(Color.WHITE);
         c.add(screenMessage);
         JPanel panSouth = new JPanel();
         panSouth.setLayout(new FlowLayout());
@@ -45,6 +81,7 @@ public class PlayerFrame extends JFrame {
         enteredChoice = new JTextField(3);
         btnSubmit = new JButton("Submit choice");
         btnNext = new JButton("Next");
+        btnNext.setEnabled(false);
         panSouth.add(yourChoice);
         panSouth.add(enteredChoice);
         panSouth.add(btnSubmit);
