31b630fa0706f4badc16441778a35f8a5bce235d
Brian <bfeddes@hotmail.com>
2022-12-12 18:50:04 -0600

    Tested Player. Works.

--- OOPHomeworkAssignments/src/createaplayer/App.java
+++ OOPHomeworkAssignments/src/createaplayer/App.java
@@ -1,7 +1,16 @@
 package createaplayer;
-
+import java.util.ArrayList;
 public class App {
     public static void main(String[] args) {
-        
+        ArrayList<Player> myPlayers = new ArrayList<Player>();
+        Player myPlayer = new Player("Brian", "Forward", 78, 185.4, 86);
+        Player myPlayer1 = new Player("Br1ian", "Forward", 78, 185.4, 86);
+        Player myPlayer2 = new Player("Braban", "Forward", 78, 185.4, 86);
+        myPlayers.add(myPlayer);
+        myPlayers.add(myPlayer1);
+        myPlayers.add(myPlayer2);
+        for (Player player : myPlayers) {
+            System.out.println(player);
+        }
     }
 }
