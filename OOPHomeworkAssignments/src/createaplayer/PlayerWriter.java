1715fd5d7939029fe15347c562a4f36414ad6717
Brian <bfeddes@hotmail.com>
2022-12-14 17:58:25 -0600

    Can write to text files.

--- OOPHomeworkAssignments/src/createaplayer/PlayerWriter.java
+++ OOPHomeworkAssignments/src/createaplayer/PlayerWriter.java
@@ -1,9 +1,25 @@
 package createaplayer;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 public class PlayerWriter {
-    /*
-     * ADD CODE FOR TEXT WRITER
-     */
+
+    public static boolean writeToText(ArrayList<Player> myPlayers, File f) {
+        try {
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(f)));
+            for (Player p : myPlayers){
+            pw.println(p);
+            }
+            pw.close();
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
 
     /*
      * ADD CODE FOR Binary WRITER
